480650870a5a6a89e6aed5ef64276d59eb087f04
liuli <vampire_624@yeah.net>
2018-12-21 15:22:58 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = require('./router/index.js')
+const app = new express()
+
+app.use(express.static(__dirname))
+app.use('/api', router)
+
+app.listen(3000, () => {
+  console.log('LAB app is running at port 3000')
+})
+
