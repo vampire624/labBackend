7b04258a61d803ceaf0b814b49245b91b934db3e
liuli <vampire_624@yeah.net>
2018-12-26 15:33:15 +0800

    add wrong router handler for 404 page

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const router = require('./router/index.js')
+const fs = require('fs')
+
 const app = new express()
-
 app.use(express.static(__dirname))
-app.use('/api', router)
-
+app.use('/api', router) // 所有错误路由返回index.html，404页面由前端路由展示
+app.use("*", (req, res) => {
+  fs.readFile('./index.html', 'utf-8', (err, chunk) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.writeHead(200, {
+       'Content-Type': 'text/html; charset=utf-8'
+      })
+      res.end(chunk)
+    }
+  })
+})
 app.listen(3000, () => {
   console.log('LAB app is running at port 3000')
 })
